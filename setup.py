bec57bc6f49043a29e8f02d08866dc893f889b6a
luricodes <github@lucasrichert.tech>
2024-10-17 14:18:44 +0200

    Projektinfos hinzugefügt

--- setup.py
+++ setup.py
@@ -3,15 +3,26 @@ from typing import Optional, List, Dict, Any
 
 setup(
     name="repo_analyzer",
-    version="0.1",
+    version="0.9.0",
+    author="Lucas Richert",
+    author_email="info@lucasrichert.tech",
+    description="Ein Tool zur Analyse von Repository-Strukturen",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
     packages=find_packages(),
     install_requires=[
-        "colorama",
-        "dicttoxml",
-        "python-magic",
-        "PyYAML",
-        "tqdm",
+        "colorama>=0.4.4",
+        "dicttoxml>=1.7.4",
+        "python-magic>=0.4.24",
+        "PyYAML>=5.4.1",
+        "tqdm>=4.60.0",
     ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
     entry_points={
         "console_scripts": [
             "repo_analyzer=repo_analyzer.main:run",
