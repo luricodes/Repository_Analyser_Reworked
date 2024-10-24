dc1b1820c8ea1479e85f56d3168980648ffc6bf7
luricodes <github@lucasrichert.tech>
2024-10-24 22:34:39 +0200

    added requirements

--- setup.py
+++ setup.py
@@ -1,38 +1,53 @@
-# setup.py
-
 from setuptools import setup, find_packages
-from typing import Optional, List, Dict, Any
 
-with open('README.md', 'r', encoding='utf-8') as fh:
-    long_description = fh.read()
+# Basic platform-independent requirements
+install_requires = [
+    "colorama>=0.4.6",
+    "dicttoxml>=1.7.16",
+    "PyYAML>=6.0.1",
+    "tqdm>=4.65.0",
+    "charset-normalizer>=3.2.0",
+    "msgpack>=1.0.5",
+    "typing-extensions>=4.7.1",
+    "pathspec>=0.11.2",
+    "rich>=13.5.2",
+    "click>=8.1.7",
+    "python-magic-bin>=0.4.14; platform_system == 'Windows'",
+    "python-magic>=0.4.27; platform_system != 'Windows'"
+]
+
+# Development dependencies
+extras_require = {
+    'dev': [
+        'pytest>=7.4.0',
+        'pytest-cov>=4.1.0',
+        'black>=23.7.0',
+        'isort>=5.12.0',
+        'mypy>=1.4.1',
+        'flake8>=6.1.0',
+    ]
+}
 
 setup(
     name="repo_analyzer",
     version="0.9.0",
     author="Lucas Richert",
     author_email="info@lucasrichert.tech",
-    description="Ein Tool zur Analyse von Repository-Strukturen",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
+    description="A tool for analyzing repository structures",
     packages=find_packages(),
-    install_requires=[
-        "colorama>=0.4.4",
-        "dicttoxml>=1.7.4",
-        "python-magic-bin>=0.4.14",
-        "PyYAML>=5.4.1",
-        "tqdm>=4.60.0",
-        "charset-normalizer>=3.2.0",
-        "msgpack>=1.1.0"
-    ],
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.13',
+    python_requires='>=3.9',
+    install_requires=install_requires,
+    extras_require=extras_require,
     entry_points={
         "console_scripts": [
             "repo_analyzer=repo_analyzer.main:run",
         ],
     },
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
